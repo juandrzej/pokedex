736ad9ebbe6effe705ee6055b2a0b4d60413209b
juandrzej <programming.yippee729@passinbox.com>
2025-11-02 16:20:36 +0100

    rm session from git, create whole cache packages, start work on tests

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -10,7 +10,53 @@ type cacheEntry struct {
 	val       []byte
 }
 
-type Cashe struct {
-	list map[string]cacheEntry
-	mu   sync.Mutex
+type Cache struct {
+	list     map[string]cacheEntry
+	mu       sync.Mutex
+	interval time.Duration
+}
+
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{
+		list:     make(map[string]cacheEntry),
+		mu:       sync.Mutex{},
+		interval: interval,
+	}
+	go c.reapLoop()
+	return c
+}
+
+func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		c.mu.Lock()
+		for k, entry := range c.list {
+			if c.interval < time.Since(entry.createdAt) {
+				delete(c.list, k)
+			}
+		}
+		c.mu.Unlock()
+
+	}
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.list[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, ok := c.list[key]
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
 }
